f4fe7abe9f026ee6b75f2fcf3cd6f7e73149198d
Luan Sousa <=>
2018-06-30 00:51:52 -0300

    m

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,71 @@
 module.exports = function(app){
-    var Tasks = app.src.db.models.Tasks;
-    app.get('/tasks', function(req, resp){
-        Tasks.findAll({}).then(function(tasks){
-            resp.json({tasks: tasks});
+    const Tasks = app.src.db.models.Tasks;
+
+    app.route("/tasks")
+        .get((req, resp) => {
+            //tasks: Listar tarefas
+            Tasks.findAll({})
+                .then(result => resp.json(result))
+                .catch(error => {
+                    resp.status(412).json({
+                        msg: error.message
+                    })
+                })
         })
-    })
+        .post((req, resp) => {
+            //tasks: Cadastrar uma tarefa
+            Tasks.create(req.body)
+                .then(result => {
+                    resp.json(result)
+                })
+                .catch(error => {
+                    resp.status(412).json({
+                        error: error.message
+                    })
+                })
+        });
+
+    app.route("/tasks/:id")
+        .get((req, resp) => {
+            //Consulta uma tarefa por id
+            Tasks.findOne({
+                    where: req.params
+                })
+                .then(result => {
+                    if (result) {
+                        resp.json(result)
+                    } else {
+                        resp.sendStatus(404)
+                    }
+                })
+                .catch(error => {
+                    resp.status(412).json({
+                        msg: error.message
+                    })
+                })
+        })
+        .put((req, resp) => {
+            //Atualiza uma tarefa por id 
+            Tasks.update(req.body, {
+                    where: req.params
+                })
+                .then(result => resp.sendStatus(204))
+                .catch(error => {
+                    resp.status(412).json({
+                        msg: error.message
+                    })
+                })
+        })
+        .delete((req, resp) => {
+            //Deleta uma tarefa por id
+            Tasks.destroy({
+                    where: req.params.id
+                })
+                .then(result => resp.sendStatus(204))
+                .catch(error => {
+                    resp.status(412).json({
+                        msg: error.message
+                    })
+                })
+        })
 }
---
+++ src/routes/users.js
@@ -0,0 +1,39 @@
+module.exports = app => {
+    const Users = app.src.db.models.Users;
+
+    app.get("/users/:id", (req, resp) => {
+        Users.findById(req.params.id, {
+                attributes: ["id, name, email"]
+            })
+            .then(result => resp.json(result))
+            .catch(error => {
+                resp.status(412).json({
+                    msg: error.message
+                })
+            })
+    });
+
+    app.delete("/users/:id", (req, resp) => {
+        Users.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(result => resp.sendStatus(204))
+            .catch(error => {
+                resp.status(412).json({
+                    msg: error.message
+                })
+            })
+    });
+
+    app.post("/users", (req, resp) => {
+        Users.create(req.body)
+            .then(result => resp.json(result))
+            .catch(error => {
+                resp.status(412).json({
+                    msg: error.message
+                })
+            })
+    });
+}
